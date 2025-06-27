3c301a442f0b27fe725b9b385352885736896f0a
Wayne Kimutai <96531439+Virtuoso1@users.noreply.github.com>
2025-06-27 20:34:56 +0300

    suppressing import warnings

--- demo/src/main/java/com/example/demo/model/CartItem.java
+++ demo/src/main/java/com/example/demo/model/CartItem.java
@@ -1,8 +1,11 @@
 package com.example.demo.model;
 
 public class CartItem {
+    @SuppressWarnings("unused")
     private Drink drink;
+    @SuppressWarnings("unused")
     private int quantity;
+    @SuppressWarnings("unused")
     private double subtotal;
 
     public CartItem(Drink drink, int quantity) {

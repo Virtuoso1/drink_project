b7ead44b31f93d9d85b58a39008946810d905ce7
Wayne Kimutai <96531439+Virtuoso1@users.noreply.github.com>
2025-07-01 23:40:08 +0300

    f

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 /*Only controlls routing to the landing page.
-I tried to move it to the Auth controller to reduce boilerplate but everything broke down.
+I tried to move it to the Auth controller to 
+reduce boilerplate but everything broke down.
 DO NOT TOUCH THIS FILE.
 WITHOUT IT THE APP DOESNT LAUNCH
 */
